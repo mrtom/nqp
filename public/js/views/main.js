9c9c9da789046cf1e71136e5ff8d87b3ee6e97e4
Tom Elliott <mrtom@cantab.net>
2012-10-18 17:51:02 +0100

    Added database and api for retrieving code

--- public/js/views/main.js
+++ public/js/views/main.js
@@ -22,47 +22,48 @@ function($, _, Backbone, Bootstrap, qr, mainTemplate) {
 
           var view = this;
 
-          FB.getLoginStatus(function(response) {
-            if (response.status === 'connected') {
-              // We handle this in the event subscription
-            } else {
-              console.debug('Must login!');
-              window.FB.XFBML.parse($('#loginModal')[0]);
-              $("#loginModal").modal();
+          FB.Event.subscribe('auth.statusChange', _.bind(function(response) {
+            switch(response.status) {
+              case 'unknown':
+                // Fall through
+              case 'not_authorized':
+                console.debug('Must login!');
+                window.FB.XFBML.parse($('#loginModal')[0]);
+                $("#loginModal").modal();
+                break;
+              case 'connected': 
+                $('#loginModal').modal('hide');
+
+                var uid = response.authResponse.userID;
+                var signedRequest = response.authResponse.signedRequest;
+                console.debug('woop! Welcome user #'+uid);
+
+                FB.api('me?fields=id,name,picture.type(square)', _.bind(function(r) {
+                  this.model.set({
+                    'uid': uid,
+                    'name': r.name,
+                    'pic': r.picture.data.url,
+                    'loaded': true,
+                    'signedRequest': signedRequest
+                  });
+
+                  $.ajax({
+                    url: "/api/gen",
+                    context: this,
+                    type: "POST",
+                    data: 'signed_request='+signedRequest,
+                  }).done(function(r) {
+                    if (r.code) {
+                      this.model.set('code', r.code);
+                    } else {
+                      // TODO: Error!
+                    }
+                  });
+                  }, this));
+                break;
+                default:
+                  console.log("Unexpected responsen from Facebook auth: `" + response.status + "` not recognised!")
             }
-          });
-
-          FB.Event.subscribe('auth.authResponseChange', _.bind(function(response) {
-            $('#loginModal').modal('hide');
-
-            var uid = response.authResponse.userID;
-            var signedRequest = response.authResponse.signedRequest;
-            console.debug('woop! Welcome user #'+uid);
-
-            FB.api('me?fields=id,name,picture.type(square)', _.bind(function(r) {
-              this.model.set({
-                'uid': uid,
-                'name': r.name,
-                'pic': r.picture.data.url,
-                'loaded': true,
-                'signedRequest': signedRequest
-              });
-
-              $.ajax({
-                url: "/api/gen",
-                context: this,
-                type: "POST",
-                data: 'signed_request='+signedRequest,
-              }).done(function(r) {
-                if (r.code) {
-                  this.model.set('code', r.code);
-                } else {
-                  // TODO: Error!
-                }
-
-              });
-
-            }, this));
           }, this));
 
         }, this));
--- server.js
+++ server.js
@@ -17,17 +17,24 @@
  var SECRET = "fcda08e9fa30627468a998425ec6a988";
 
 var express = require('express'),
-    uuid = require('node-uuid'),
-    config = require('./config'),
-    crypto = require('crypto');
+    uuid    = require('node-uuid'),
+    config  = require('./config'),
+    crypto  = require('crypto'),
+    restler = require('restler'),
+    qs      = require('querystring'),
+    sqlite3 = require('sqlite3');
 
 var app = express.createServer();
+var db  = new sqlite3.Database('./db/master.sqlite');
 
 app.configure(function() {
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  
   app.use(express.static(__dirname + '/public'));
   
   app.post('/api/gen', function(req, res) {
+    console.log("Received new API call to /api/gen at " + new Date());
 
     // Decode signed Request
     var signedRequest = req.param('signed_request');
@@ -37,17 +44,12 @@ app.configure(function() {
         var encodedSig = signedRequest.substr(0, index);
         var payload = signedRequest.substr(index+1);
 
-        // Do some substitutions
-        payload = payload.replace("-", "+");
-        payload = payload.replace("_", "/");
-
         var data = JSON.parse(new Buffer(payload, 'base64').toString('ascii'));
-
         if (data.algorithm.toUpperCase() !== 'HMAC-SHA256') {
           // TODO: Handle error, log etc
         }
 
-        var hashedSig = crypto.createHmac('SHA256', config.app.secret).update(payload).digest('base64');
+        var hashedSig = crypto.createHmac('SHA256', config.app.secret).update(payload).digest('base64').replace(/\+/g,'-').replace(/\//g,'_').replace('=','');
 
         var decodedSig = new Buffer(encodedSig, 'base64').toString('ascii');
         var decodedHashedSig = new Buffer(hashedSig, 'base64').toString('ascii');
@@ -56,24 +58,90 @@ app.configure(function() {
           console.log("*** WARNING!: Couldn't verify signed request! ***")
         }
 
-        // Extract access token and generate our code
-        var encodedCode = crypto.createHmac('SHA256', SECRET).update(data.user_id).digest('base64');
+        // Get access token
+        var access_token;
+        var expires;
+        var userId = data.user_id;
 
-        // TODO:
-        // Save user_id into database along with access token
+        if (data.oauth_token) {
+          access_token = data.oauth_token;
+          expires = data.expires;
+        } else {
+          if (!data.code) {
+            // throw("no oauth token and no code to get one");
+            // TODO: Handle error case
+            console.log("*** WARNING!: No ouath token and no code to get one. :( ***");
+          }
 
-        res.header('Content-Type', 'text/json');
-        res.send(JSON.stringify({
-          'success': true,
-          'code': encodedCode
-        }));
-        return;
+          var params = {
+            client_id:     config.app.app_id,
+            client_secret: config.app.secret,
+            redirect_uri:  '',
+            code:          data.code
+          };
+
+          var request = restler.get('https://graph.facebook.com/oauth/access_token', { query:params });
+          
+          request.on('fail', function(data) {
+            var result = JSON.parse(data);
+            console.log('Invalid code: ' + result.error.message);
+            sendInternalServerError();
+          });
+
+          request.on('success', function(r) {
+            access_token = qs.parse(r).access_token;
+            expires = qs.parse(r).expires;
+
+            // Extract access token and generate our code
+            var encodedCode = crypto.createHmac('SHA256', SECRET).update(data.user_id).digest('base64');
+
+            // Save everything to our database
+            createOrUpdateUserRecord(userId, access_token, expires, encodedCode, function(db_success) {
+              if (db_success) {
+                res.header('Content-Type', 'text/json');
+                res.send(JSON.stringify({
+                  'success': true,
+                  'code': encodedCode
+                }));
+              } else {
+                sendInternalServerError();
+              }
+              return;
+            });
+          });
+        }
       }
     }
+  });
 
-    // TODO: Handle error more gracefully
-    res.header('Content-Type', 'text/json');
-    res.write(JSON.stringify({'success': false}));
+  app.get('/api/get_access_token', function(req, res) {
+    console.log("Received new API call to /api/get_access_token at " + new Date());
+
+    var hash = req.query["code"];
+    if (hash) {
+      db.all("SELECT access_token AS token, access_token_expires AS expires FROM user WHERE hash = $hash", {
+        $hash: hash
+      }, function(err, rows) {
+        if (err || rows.length > 1) {
+          sendInternalServerError();
+        } else {
+          res.header('Content-Type', 'text/json');
+          if (rows.length == 0) {
+            res.send(JSON.stringify({
+              'success': true
+            }));
+          } else {
+            res.send(JSON.stringify({
+              'success': true,
+              'access_token': rows[0].token,
+              'expires': rows[0].expires
+            }));
+          }
+        }
+      });
+    } else {
+      sendInternalServerError();
+    }
   });
 
   app.use(function(req, res, next){
@@ -82,6 +150,47 @@ app.configure(function() {
     res.sendfile('public/index.html');
   });
 });
+
+function sendInternalServerError() {
+  res.header('Content-Type', 'text/json');
+  res.status(500);
+  res.write(JSON.stringify({'success': false}));
+};
+
+function createOrUpdateUserRecord(fb_id, access_token, access_token_expires, hash, cb) {
+  db.get("SELECT count(*) AS total FROM user WHERE fbid = $fb_id", {
+    $fb_id: fb_id
+  }, function(err, result) {
+    if (result.total > 1) {
+      // TODO: Handler error
+      cb(false);
+    } else if (result.total == 0) {
+      // Create a new row
+      console.log('Creating new row in Database for user ' + fb_id);
+      db.run("INSERT INTO user (fbid, access_token, access_token_expires, hash) VALUES ($fb_id, $access_token, $access_token_expires, $hash)", {
+        $fb_id: fb_id,
+        $access_token: access_token,
+        $access_token_expires: access_token_expires,
+        $hash: hash
+      }, function(err) {
+        if (err) cb(false);
+        else cb(true);
+      });
+    } else {
+      // Update row
+      console.log('Updating user ' + fb_id + ' in Database');
+      db.run("UPDATE user SET fbid = $fb_id, access_token = $access_token, access_token_expires = $access_token_expires, hash = $hash WHERE fbid = $fb_id", {
+        $fb_id: fb_id,
+        $access_token: access_token,
+        $access_token_expires: access_token_expires,
+        $hash: hash
+      }, function(err) {
+        if (err) cb(false);
+        else cb(true);
+      })
+    }
+  });
+};
 
 // sigh no ipv6
 // Do some shuffling for heroku vs localhost
